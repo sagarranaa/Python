e9c5f862716367117e19de1c707423b69ea9e23d
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2020-12-16 18:52:39 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,17 @@
+# list or array(list is mutalable)
+num=[25,458,7565,7,556687,54]
+print(num)
+
+# mixing list
+value=['sonali','sonam','neha',2,5,8,7,1,5]
+# print(value[3:])
+
+## inbulid function
+# value.append(9) #append is inbulid function will end to list
+# print(value)
+value.insert(3,11) ## insert will add in any specific position
+# print(value)
+
+value.remove(2)
+
+print(value)
