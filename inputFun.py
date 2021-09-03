fb74c651c42f1ed318b413b67ec22aad0b452c04
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2021-09-03 21:05:44 +0530

    Add files via upload

---
+++ inputFun.py
@@ -0,0 +1,15 @@
+# x= int(input("please enter a first value:"))
+# y=int(input ("please enter second value:"))
+# c=x+y
+# print(c)
+
+# another code
+# ch=input("Enter a character:")[0]
+# print(ch[0])
+# print(ch)
+# evaluate function
+
+# excercise
+x=int(input("enter the value:"))
+y=x**3
+print(y)
