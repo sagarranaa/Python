a2b9a7e9d9a78a5276f447e50e7e82fb9d3ec9f6
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2022-01-08 07:41:09 -0500

    Add files via upload

--- DataType.py
+++ DataType.py
@@ -1,48 +1,18 @@
-# Datatype is python
-# - None
-# - numeric(int,float,complex,bool)
-# -list
-# -tuple
-# -set
-# -string
-# -Range
-# -Dictionary
-# a=2.5
-# print(type(a))
-# a=True
-# print(type(a))
-
-# how to convert float value to integer
-# a=int(2.5)
-# print(type(a))
-
-
-
-# b=float(10)
-# c=15
-
-
-# print(type(b))
-
-# boolean
-
-# print(b>c)
-# print(b==c)
-
-# List 
-
-# range(0,100)
-# print(list(range(100)))
-
-##Even number 
-# range(0,100,2)
-# print("This is Even Number",set(range(0,100,2)))
-
-d={
-	'Chhotu':"motoG5",'sagar':'Redmi5A','sonam':'Sumsung M31','Naveen':'Realme X2','Mitilesh':'Realme XT','babbu':'Redmi 5A',
-	'papa':'Realme'
-}
-# print(d.values())
-# print(d.keys())
-# print(d['sonam'])
-print(d.get('Naveen'))
+""" Datatypes in python
+1. None
+2.Numeric- int,float,complex,bool
+3.List
+4.Tuple
+5.Set
+6.String
+7.Range
+9.Dictionary
+"""
+" example"
+n=2.04
+print(type(n))
+num=6+9j
+print(type(num))
+k=print(int(n))
+"Range function "
+print(tuple(range(0,20,2)))
---
+++ practmorevarible.py
@@ -0,0 +1,6 @@
+""" More varible in python"""
+
+a=1
+b=a
+print(id(a))
+print(id(b))
